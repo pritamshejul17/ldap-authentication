747033e1315630a381e61ed91a1bc96601108b65
Pritam Shejul <117113144+pritamshejul17@users.noreply.github.com>
2023-03-16 20:38:28 +0530

    Made changes to application.properties

---
+++ src/main/java/security/io/ldapauthentication/LdapAuthenticationApplication.java
@@ -0,0 +1,13 @@
+package security.io.ldapauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LdapAuthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LdapAuthenticationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/security/io/ldapauthentication/LdapAuthenticationApplicationTests.java
@@ -0,0 +1,13 @@
+package security.io.ldapauthentication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LdapAuthenticationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
